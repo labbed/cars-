1f30c8ad305e19ee4b585937fcbbb425d4d8efbf
hamza labbed <hamzalabb@gmail.com>
2023-01-21 11:39:16 +0100

    gg

---
+++ src/components/Londing.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Londing() {
+  return (
+    <div>
+      <img
+        className="lodding"
+        src="https://mdrndealership.netlify.app/static/media/car-2.7aac585b.jpg"
+        alt=""
+      />
+    </div>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function navbar() {
+  return (
+    <div>
+      <ul className="nav">
+        <img className="png" src="logo1.png" alt="" />
+        <h3>Home</h3>
+        <h3>Cars</h3>
+      </ul>
+    </div>
+  );
+}
